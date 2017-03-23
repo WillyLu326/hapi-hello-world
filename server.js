6ee888be13b22e31d582dd710ea702130e21688a
WillyLu326 <luzh0326@gmail.com>
2017-03-22 21:53:37 -0400

    add airplane data

--- server.js
+++ server.js
@@ -3,25 +3,7 @@ const server = new Hapi.Server();
 
 server.connection({ port: 3001 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    config: {
-        handler: (req, res) => {
-            res({ message: 'This is home page' })
-        }
-    }
-});
 
-server.route({
-    method: 'GET',
-    path: '/hello',
-    config: {
-        handler: (req, res) => {
-            res({ message: 'This is hello page' })
-        }
-    }
-})
 
 server.start(err => {
     if (err) throw err;
